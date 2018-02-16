9bf5ff14c94fcdb58b88bb4d91ad06ed7a691cbd
masihNoob <neraka.malam@gmail.com>
2018-02-16 07:10:22 +0700

    ujicoba

---
+++ Assets/Script/TongSampahController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TongSampahController : MonoBehaviour {
+
+	public float speed = 0.0f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey(KeyCode.A)){
+			//kiri
+			GoLeft();
+		}else if(Input.GetKey(KeyCode.D)){
+			//kanan
+			GoRight();
+		}
+	}
+
+	private void GoLeft(){
+		transform.Translate(Vector3.left * speed* Time.deltaTime);
+	}
+	private void GoRight(){
+		transform.Translate(Vector3.right * speed* Time.deltaTime);
+	}
+}
+
+/*
+1. tong sampah dapat bergerak
+2. sampah == tong sampah "skor bertambah"
+3. lantai = 5 detik hilang
+ */
